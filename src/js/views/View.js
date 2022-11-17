c9536b4d0d972321af74659593f4532c64bf383f
ipcampbell12 <ipcampbell12@gmail.com>
2022-11-16 17:24:22 -0800

    added some docs

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -4,6 +4,16 @@ import icons from 'url:../../img/icons.svg';
 export default class View {
   _data;
 
+
+  /**
+   * Redner the recieved object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM 
+   * @returns {undefined | string } A markup string is returned if render is false
+   * @this {Object} View instance  
+   * @author Ian Campbell
+   * @todo Finish implementation
+   */
   //data stored in this.data
   render(data, render = true) {
 
